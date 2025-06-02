1bc738232c37bf7070c0763d7f400a659bbadc3c
Petr Popov <popov.pe@gmai.com>
2025-06-02 13:34:30 +0300

    adds generic component FilterOptionAsMenu

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,17 +1,29 @@
-import { Text, List, HStack, Avatar, Button, Spinner, Menu, Portal } from "@chakra-ui/react";
+import {
+  Text,
+  List,
+  HStack,
+  Avatar,
+  Button,
+  Spinner,
+  Menu,
+  Portal,
+} from "@chakra-ui/react";
 import useGenre from "../hooks/useGenre";
 import { FC, useState } from "react";
 import { Genre } from "../model/fetch-genre-types";
 import { FaChevronDown, FaChevronUp } from "react-icons/fa";
 import MotionComponent from "./MotionComponent";
 
-
 interface GenreListProps {
   selectedGenre: string | null;
   genres: Genre[];
-  onSelectGenre: (genre: string)=>void;
+  onSelectGenre: (genre: string) => void;
 }
-const GenreAsList: FC<GenreListProps> = ({selectedGenre, genres, onSelectGenre}) => {
+const GenreAsList: FC<GenreListProps> = ({
+  selectedGenre,
+  genres,
+  onSelectGenre,
+}) => {
   return (
     <List.Root listStyle="none" maxHeight="85vh" overflow="auto">
       {genres.map((g) => (
@@ -19,105 +31,178 @@ const GenreAsList: FC<GenreListProps> = ({selectedGenre, genres, onSelectGenre})
           <HStack padding={2}>
             <Avatar.Root shape="rounded" size="lg">
               <Avatar.Fallback name={g.name} />
-              <Avatar.Image src={g.image_background}/>
+              <Avatar.Image src={g.image_background} />
             </Avatar.Root>
-            <Button {...getSelectedStyls(g.slug, selectedGenre)} variant={"outline"} borderWidth="0" fontSize={"1.1rem"} paddingX="1"
-            onClick={()=>onSelectGenre(g.slug)}>{g.name}</Button>
+            <Button
+              {...getSelectedStyls(g.slug, selectedGenre)}
+              variant={"outline"}
+              borderWidth="0"
+              fontSize={"1.1rem"}
+              paddingX="1"
+              onClick={() => onSelectGenre(g.slug)}
+            >
+              {g.name}
+            </Button>
           </HStack>
         </List.Item>
       ))}
     </List.Root>
-  )
-}
+  );
+};
 
+interface MenuItem {
+  slug: string;
+  name: string;
+}
 interface FilterOptionAsMenuProps<T> {
   selectedItem: T | null;
   items: T[];
-  onSelect: (Item: T)=>void;
-  optionName: string
+  onSelect: (Item: T) => void;
+  optionName: string;
 }
-const FilterOptionAsMenu: FC<FilterOptionAsMenuProps<T>> = ({selectedItem, items, onSelect, optionName}) => {
-  const duration=0.7;
-  const [isOpen, setIsOpen] =  useState<boolean>(false)
+const FilterOptionAsMenu = <T extends MenuItem>({
+  selectedItem,
+  items,
+  onSelect,
+  optionName,
+}: FilterOptionAsMenuProps<T>) => {
+  const duration = 0.7;
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   return (
     <Menu.Root onExitComplete={() => setIsOpen(false)}>
       <Menu.Trigger asChild>
-        <Button variant="outline" size="sm" marginBottom={3} onClick={() => setIsOpen(!isOpen)}>
-         { selectedItem || optionName}
-          {isOpen ? <MotionComponent duration={duration}>
-            <FaChevronUp></FaChevronUp>
-          </MotionComponent> :<FaChevronDown></FaChevronDown>}
+        <Button
+          variant="outline"
+          size="sm"
+          marginBottom={3}
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {selectedItem?.name || optionName}
+          {isOpen ? (
+            <MotionComponent duration={duration}>
+              <FaChevronUp></FaChevronUp>
+            </MotionComponent>
+          ) : (
+            <FaChevronDown></FaChevronDown>
+          )}
         </Button>
       </Menu.Trigger>
       <Portal>
         <Menu.Positioner>
           <MotionComponent duration={duration}>
             <Menu.Content>
-              {items.map(p => <Menu.Item key={p.slug} value={p.slug}
-               onClick={() => {onSelect(p.slug); setIsOpen(false)}}>{p.name}</Menu.Item>)}
+              {items.map((p) => (
+                <Menu.Item
+                  key={p.slug}
+                  value={p.slug}
+                  onClick={() => {
+                    onSelect(p);
+                    setIsOpen(false);
+                  }}
+                >
+                  {p.name}
+                </Menu.Item>
+              ))}
             </Menu.Content>
           </MotionComponent>
         </Menu.Positioner>
       </Portal>
     </Menu.Root>
-  )
-}
+  );
+};
 
-const GenreAsMenu: FC<GenreListProps> = ({selectedGenre, genres, onSelectGenre}) => {
-  const duration=0.7;
-  const [isOpen, setIsOpen] =  useState<boolean>(false)
+const GenreAsMenu: FC<GenreListProps> = ({
+  selectedGenre,
+  genres,
+  onSelectGenre,
+}) => {
+  const duration = 0.7;
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   return (
     <Menu.Root onExitComplete={() => setIsOpen(false)}>
       <Menu.Trigger asChild>
-        <Button variant="outline" size="sm" marginBottom={3} onClick={() => setIsOpen(!isOpen)}>
-         { selectedGenre || "Genres"}
-          {isOpen ? <MotionComponent duration={duration}>
-            <FaChevronUp></FaChevronUp>
-          </MotionComponent> :<FaChevronDown></FaChevronDown>}
+        <Button
+          variant="outline"
+          size="sm"
+          marginBottom={3}
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {selectedGenre || "Genres"}
+          {isOpen ? (
+            <MotionComponent duration={duration}>
+              <FaChevronUp></FaChevronUp>
+            </MotionComponent>
+          ) : (
+            <FaChevronDown></FaChevronDown>
+          )}
         </Button>
       </Menu.Trigger>
       <Portal>
         <Menu.Positioner>
           <MotionComponent duration={duration}>
             <Menu.Content>
-              {genres.map(p => <Menu.Item key={p.slug} value={p.slug}
-               onClick={() => {onSelectGenre(p.slug); setIsOpen(false)}}>{p.name}</Menu.Item>)}
+              {genres.map((p) => (
+                <Menu.Item
+                  key={p.slug}
+                  value={p.slug}
+                  onClick={() => {
+                    onSelectGenre(p.slug);
+                    setIsOpen(false);
+                  }}
+                >
+                  {p.name}
+                </Menu.Item>
+              ))}
             </Menu.Content>
           </MotionComponent>
         </Menu.Positioner>
       </Portal>
     </Menu.Root>
-  )
-}
+  );
+};
 export type GenreComponentVariants = keyof typeof GanreRenderVariants;
 
-function getSelectedStyls(slug: string, selectedGenre: string | null) : {fontWeight: string, color: string} {
-     return slug === selectedGenre ? {fontWeight: "bold", color: "red"}: {fontWeight: "normal", color: "initial"}
+function getSelectedStyls(
+  slug: string,
+  selectedGenre: string | null
+): { fontWeight: string; color: string } {
+  return slug === selectedGenre
+    ? { fontWeight: "bold", color: "red" }
+    : { fontWeight: "normal", color: "initial" };
 }
 const GanreRenderVariants = {
   list: GenreAsList,
-  menu: GenreAsMenu
- } 
- interface Props {
+  menu: GenreAsMenu,
+};
+interface Props {
   onSelectGenre: (genreSlug: string) => void;
   selectedGenre: string | null;
   componentType: GenreComponentVariants;
-  addShowAllItem: boolean
+  addShowAllItem: boolean;
 }
-const GenreList: FC<Props> = ({onSelectGenre, selectedGenre, componentType, addShowAllItem}) => {
- const ComponentToRender = GanreRenderVariants[componentType];
- console.log(addShowAllItem);
- const {data: genres, error, isLoading} = useGenre(addShowAllItem);
- 
- return  (
+const GenreList: FC<Props> = ({
+  onSelectGenre,
+  selectedGenre,
+  componentType,
+  addShowAllItem,
+}) => {
+  const ComponentToRender = GanreRenderVariants[componentType];
+  console.log(addShowAllItem);
+  const { data: genres, error, isLoading } = useGenre(addShowAllItem);
+
+  return (
     <>
-    {isLoading && <Spinner></Spinner>}
-      {error? (
+      {isLoading && <Spinner></Spinner>}
+      {error ? (
         <Text color="red" fontSize={"2.5rem"}>
           {error}
         </Text>
       ) : (
-        <ComponentToRender genres={genres} onSelectGenre={onSelectGenre} selectedGenre={selectedGenre} />
+        <ComponentToRender
+          genres={genres}
+          onSelectGenre={onSelectGenre}
+          selectedGenre={selectedGenre}
+        />
       )}
     </>
   );
